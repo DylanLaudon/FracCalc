15b26000058a563973221b22e432785dbec1e980
DylanLaudon <550073@my.cuhsd.org>
2022-11-15 10:04:32 -0800

    Updated May need to move parts of produce answer

--- FracCalc/src/src/FracCalc.java
+++ FracCalc/src/src/FracCalc.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-        System.out.println(FracCalc.produceAnswer);
+        System.out.println(FracCalc.produceAnswer());
     }
-    
+
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
     //
@@ -17,8 +16,7 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
+    public static String produceAnswer(String input) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Input your first fraction: ");
@@ -38,13 +36,11 @@ public class FracCalc {
         int denominator2 = workv2.nextInt();
         Fraction objfrac2 = new Fraction(numerator2, denominator2);
 
-        // TODO: Implement this function to produce the solution to the input
-        public static FracCalc produceAnswer {
-        int newNum = (objfrac1.getNumerator() * objfrac2.getDenominator()) + (objfrac2.getNumerator() * objfrac1.getDenominator());
-        int newDen = (objfrac1.getDenominator() * objfrac2.getDenominator());
-        return newNum + " / " + newDen;
+
+            int newNum = (objfrac1.getNumerator() * objfrac2.getDenominator()) + (objfrac2.getNumerator() * objfrac1.getDenominator());
+            int newDen = (objfrac1.getDenominator() * objfrac2.getDenominator());
+            return newNum + " / " + newDen;
+
+        // TODO: Fill in the space below with any helper methods that you think you will need
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
 }
