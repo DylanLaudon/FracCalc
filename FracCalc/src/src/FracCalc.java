83b4d8116bdea29d202c3f32ba00205784252a1e
DylanLaudon <550073@my.cuhsd.org>
2022-11-15 09:58:44 -0800

    Updated

--- FracCalc/src/src/FracCalc.java
+++ FracCalc/src/src/FracCalc.java
@@ -6,6 +6,19 @@ public class FracCalc {
     public static void main(String[] args)
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
+        System.out.println(FracCalc.produceAnswer);
+    }
+    
+    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
+    // This function takes a String 'input' and produces the result
+    //
+    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+    //      e.g. input ==> "1/2 + 3/4"
+    //        
+    // The function should return the result of the fraction after it has been calculated
+    //      e.g. return ==> "1_1/4"
+    public static String produceAnswer(String input)
+    {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Input your first fraction: ");
@@ -25,20 +38,8 @@ public class FracCalc {
         int denominator2 = workv2.nextInt();
         Fraction objfrac2 = new Fraction(numerator2, denominator2);
 
-        System.out.println(FracCalc.produceAnswer);
-    }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
         // TODO: Implement this function to produce the solution to the input
+        public static FracCalc produceAnswer {
         int newNum = (objfrac1.getNumerator() * objfrac2.getDenominator()) + (objfrac2.getNumerator() * objfrac1.getDenominator());
         int newDen = (objfrac1.getDenominator() * objfrac2.getDenominator());
         return newNum + " / " + newDen;
